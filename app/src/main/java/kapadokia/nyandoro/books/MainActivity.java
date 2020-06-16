fd4a45bcea44e74606fbb6b85381f54efafa2a28
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-06-17 00:43:52 +0300

    used string URL to build our url

--- app/src/main/java/kapadokia/nyandoro/books/MainActivity.java
+++ app/src/main/java/kapadokia/nyandoro/books/MainActivity.java
@@ -2,7 +2,13 @@ package kapadokia.nyandoro.books;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        try {
+            URL bookUrl = ApiUtil.buildUrl("cooking");
+            new BookQueryTask().execute(bookUrl);
+        } catch (Exception e) {
+            Log.d("Error", e.toString());
+        }
+
+
+
     }
+
+    public class BookQueryTask extends AsyncTask<URL, Void, String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+
+            URL searchUrl = urls[0];
+            String result = null;
+
+            try {
+                result = ApiUtil.getJson(searchUrl);
+            }catch (Exception e){
+                Log.d("Error", e.toString());
+            }
+
+            return  result; 
+      }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView textView = findViewById(R.id.tv_response);
+            textView.setText(result);
+        }
+    }
+
 }
