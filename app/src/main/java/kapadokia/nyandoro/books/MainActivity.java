8a3233fe6ad508c9a166d7d38f8fcd4aa663a26c
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-06-28 16:48:14 +0300

    search functionality working perfectly

--- app/src/main/java/kapadokia/nyandoro/books/MainActivity.java
+++ app/src/main/java/kapadokia/nyandoro/books/MainActivity.java
@@ -1,24 +1,35 @@
 package kapadokia.nyandoro.books;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
+import kapadokia.nyandoro.books.adapter.BooksAdapter;
 import kapadokia.nyandoro.books.model.Book;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
 
     private ProgressBar progressBar;
     private TextView error_tv;
+    private RecyclerView recyclerViewBooks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
 
         progressBar = findViewById(R.id.pb_loading);
         error_tv = findViewById(R.id.error_tv);
+
+        recyclerViewBooks = findViewById(R.id.rv_books);
+        // creating a layout manager
+        LinearLayoutManager booksLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
+           recyclerViewBooks.setLayoutManager(booksLayoutManager);
 
 
         try {
@@ -38,6 +54,39 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.book_list_menu, menu);
+        final SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(true); // Do not iconify the widget; expand it by default
+
+        // responding to user action
+        searchView.setOnQueryTextListener(this);
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        try {
+            URL bookUrl = ApiUtil.buildUrl(query);
+            new BookQueryTask().execute(bookUrl);
+        }catch (Exception e){
+            Log.d("Error", "onQueryTextSubmit: " + e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 
     public class BookQueryTask extends AsyncTask<URL, Void, String>{
@@ -69,8 +118,10 @@ public class MainActivity extends AppCompatActivity {
 
 
             if (result==null){
+                recyclerViewBooks.setVisibility(View.INVISIBLE);
                 error_tv.setVisibility(View.VISIBLE);
             }else {
+                recyclerViewBooks.setVisibility(View.VISIBLE);
                 error_tv.setVisibility(View.INVISIBLE);
             }
 
@@ -79,12 +130,9 @@ public class MainActivity extends AppCompatActivity {
             // create an empty string as a container for the result
             String resultString = "";
 
-            for (Book book: books){
-                resultString = resultString + book.title + "\n"+
-                        book.publishedDate+"\n\n";
-            }
 
-
+            BooksAdapter adapter =  new BooksAdapter(books);
+            recyclerViewBooks.setAdapter(adapter);
         }
     }
 
